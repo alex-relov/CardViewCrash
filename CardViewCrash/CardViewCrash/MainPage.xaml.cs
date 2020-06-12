aacaa10a975e33e6b57ea2f3f0f40d4326502f16
alex <info@alex-relov.ru>
2020-06-12 19:14:23 +0500

    Add project files.

---
+++ CardViewCrash/CardViewCrash/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CardViewCrash
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>
+        {
+            "https://via.placeholder.com/300.png/09f/fff",
+            "https://via.placeholder.com/300.png/01f/fff"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void Select(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
